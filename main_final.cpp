48d5427a038db8237bccaccc4455b1fdcc928675
StephArn <34690734+StephArn@users.noreply.github.com>
2020-03-19 00:54:28 +0200

    Update main_final.cpp

--- main_final.cpp
+++ main_final.cpp
@@ -9,10 +9,10 @@ class Numar_Complex {
 
 public:
     // constructor de initializare:
-    Numar_Complex(double ri, double ii)
+    Numar_Complex(double re=0, double im=0)
     {
-        real = ri;
-        imaginar = ii;
+        real = re;
+        imaginar = im;
     }
     // constructor de copiere:
     Numar_Complex(const Numar_Complex& ob)
@@ -48,21 +48,22 @@ public:
     friend istream& operator>>(istream& in, Numar_Complex& nr);
     friend ostream& operator<<(ostream& out, Numar_Complex& nr);
 
-    void afisare()
+    ostream& afisare(ostream& out)
     {
         double a, b;
-        if (imaginar == 0) {
+        if (imaginar == 0)
             cout << real;
+
+        else if (real == 0)
+                cout << imaginar << "i";
+
+        else
+        {
+            cout << real <<" ";
+            if(imaginar > 0) cout<<"+";
+            cout<<imaginar<<"*i";
         }
-        if (real == 0)
-            if (imaginar > 0)
-                cout << "i*" << imaginar;
-            else
-                cout << "-i*" << (-1) * imaginar;
-        else if (imaginar > 0)
-            cout << real << " + i*" << imaginar;
-        else if (imaginar < 0)
-            cout << real << " - i*" << (-1) * imaginar;
+        return out;
     }
     double modul()
     {
@@ -71,23 +72,15 @@ public:
 
     Numar_Complex sqrt()
     {
-        Numar_Complex rad2(0, 0);
-        rad2.real = std::sqrt(
-            (std::sqrt((this->real) * (this->real) + (this->imaginar) * (this->imaginar))
-                + this->real)
-            / 2);
+        Numar_Complex rad2;
+        double m=modul();
+        rad2.real = std::sqrt((m + this->real) / 2);
 
         if (imaginar < 0) {
-            rad2.imaginar = (-1) * std::sqrt((std::sqrt((this->real) * (this->real)
-                                                  + (this->imaginar) * (this->imaginar))
-                                                 - this->real)
-                                       / 2);
+            rad2.imaginar = -1 * std::sqrt((m - this->real) / 2);
         }
         else
-            rad2.imaginar = std::sqrt(
-                (std::sqrt((this->real) * (this->real) + (this->imaginar) * (this->imaginar))
-                    - this->real)
-                / 2);
+            rad2.imaginar = std::sqrt((m - this->real) / 2);
 
         return rad2;
     }
@@ -95,9 +88,7 @@ public:
     ~Numar_Complex();
 };
 
-Numar_Complex::~Numar_Complex()
-{
-}
+Numar_Complex::~Numar_Complex() = default;
 
 istream& operator>>(istream& in, Numar_Complex& nr)
 {
@@ -107,67 +98,61 @@ istream& operator>>(istream& in, Numar_Complex& nr)
 
 ostream& operator<<(ostream& out, Numar_Complex& nr)
 {
-    nr.afisare();
-    return out;
+    return nr.afisare(out);
 }
 
 Numar_Complex operator+(Numar_Complex a, Numar_Complex b)
 {
-    Numar_Complex sum(0, 0);
-    sum.setReal((a.getReal()) + b.getReal());
-    sum.setImaginar((a.getImaginar()) + b.getImaginar());
+    Numar_Complex sum;
+    sum.setReal(a.real + b.real);
+    sum.setImaginar(a.imaginar + b.imaginar);
     return sum;
 }
 
 Numar_Complex operator*(Numar_Complex a, Numar_Complex b)
 {
-    Numar_Complex prod(0, 0);
-    if (a.getImaginar() == 0) {
-        prod.setReal((a.getReal()) * b.getReal());
-        prod.setImaginar((a.getReal()) * b.getImaginar());
+    Numar_Complex prod;
+    if (a.imaginar == 0) {
+        prod.setReal(a.real * b.real);
+        prod.setImaginar(a.real * b.imaginar);
     }
-    else if (b.getImaginar() == 0) {
-        prod.setReal((a.getReal()) * b.getReal());
-        prod.setImaginar(b.getReal() * (a.getImaginar()));
+    else if (b.imaginar == 0) {
+        prod.setReal(a.real * b.real);
+        prod.setImaginar(b.real * a.imaginar);
     }
     else {
-        prod.setReal((a.getReal()) * b.getReal() - a.getImaginar() * b.getImaginar());
-        prod.setImaginar((a.getReal()) * b.getImaginar() + b.getReal() * (a.getImaginar()));
+        prod.setReal(a.real * b.real - a.imaginar * b.imaginar);
+        prod.setImaginar(a.real * b.imaginar + b.real * a.imaginar);
     }
     return prod;
 }
 
 Numar_Complex operator*(double a, Numar_Complex b)
 {
-    Numar_Complex prodi(0, 0);
-    prodi.setReal(b.getReal() * a);
-    prodi.setImaginar(b.getImaginar() * a);
-    return prodi;
+    return Numar_Complex(b.real * a, b.imaginar * a);
 }
 Numar_Complex operator/(Numar_Complex a, Numar_Complex b)
 {
-    Numar_Complex cat(0, 0);
-    cat.setReal(((a.getReal()) * b.getReal() + (a.getImaginar()) * b.getImaginar())
-        / (b.getReal() * b.getReal() + b.getImaginar() * b.getImaginar()));
-    cat.setImaginar((b.getReal() * (a.getImaginar()) - (a.getReal()) * b.getImaginar())
-        / (b.getReal() * b.getReal() + b.getImaginar() * b.getImaginar()));
+    Numar_Complex cat;
+    cat.setReal((a.real * b.real + a.imaginar * b.imaginar)
+        / (b.real * b.real + b.imaginar * b.imaginar));
+    cat.setImaginar((b.real * a.imaginar - a.real * b.imaginar)
+        / (b.real * b.real + b.imaginar * b.imaginar));
     return cat;
 }
 
 int main()
 {
-    Numar_Complex a(0, 0), b(0, 0), c(0, 0), r(0, 0), delta(0, 0), sol1(0, 0), sol2(0, 0);
+    Numar_Complex a, b, c, r, delta, sol1, sol2;
     cin >> a;
     cin >> b;
     cin >> c;
-    delta = b * b + (-4.0) * a * c;
+    delta = b * b +(-4.0) * a * c;
     r = delta.sqrt();
-    sol1 = ((-1.0) * (b + r)) / (2.0 * a);
-    sol2 = ((-1.0) * b + r) / (2.0 * a);
-    cout << "Prima solutie: ";
-    sol1.afisare();
-    cout << endl;
-    cout << "A doua solutie: ";
-    sol2.afisare();
+    sol1 = (-1.0 * (b + r)) / (2.0 * a);
+    sol2 = (-1.0 * b + r) / (2.0 * a);
+    cout << "Prima solutie: " << sol1 << endl;
+    cout << "A doua solutie: "<< sol2;
+
     return 0;
 }

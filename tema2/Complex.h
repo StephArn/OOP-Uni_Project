6bcd0abaa8745460fe5f474fa1b2facc4055933e
StephArn <34690734+StephArn@users.noreply.github.com>
2020-05-02 00:39:42 +0300

    Update Complex.h

--- tema2/Complex.h
+++ tema2/Complex.h
@@ -1,1 +1,21 @@
+#include <iostream>
 
+class Complex
+{
+	float real, imaginar;
+public:
+    // constructor de initializare:
+    explicit Complex(float re = 0, float im = 0);
+
+    // constructor de copiere:
+    Complex(const Complex& ob);
+
+    //destuctor:
+    ~Complex();
+
+    //supraincarcare =:
+    Complex& operator=(const Complex&);
+
+    friend istream& operator >> (istream & in, Complex & nr);
+    friend ostream& operator << (ostream & out, Complex & nr);
+};

d4c58817725e4b193d347fab6200d4bd04a2b79c
StephArn <34690734+StephArn@users.noreply.github.com>
2020-05-05 02:59:57 +0300

    Create Matrice_oarecare.h

---
+++ tema2/Matrice_oarecare.h
@@ -0,0 +1,26 @@
+#pragma once
+#include <iostream>
+#include "Matrice.h"
+#ifndef TEMAPOO2_MATRICE_OARECARE_H
+#define TEMAPOO2_MATRICE_OARECARE_H
+class Matrice_oarecare :public Matrice
+{
+	int lin, col;
+    explicit Matrice_oarecare(int col = 1, int lin = 1) : Matrice(col, lin) {};
+
+    Matrice_oarecare(const Matrice_oarecare&);
+
+    ~Matrice_oarecare();
+
+    Matrice_oarecare& operator =(Matrice_oarecare& ob);
+
+    friend std::ostream& operator <<(std::ostream& output, const Matrice_oarecare& mat);
+
+    friend std::istream& operator >>(std::istream& input, Matrice_oarecare& mat);
+
+    int verifica_triunghiulara() override;
+
+    void verifica_diagonala() override;
+};
+
+#endif // !TEMAPOO2_MATRICE_OARECARE_H

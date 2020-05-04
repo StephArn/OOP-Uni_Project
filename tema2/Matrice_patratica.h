719bfe053b74878f338a366957355b325b6a415d
StephArn <34690734+StephArn@users.noreply.github.com>
2020-05-05 02:56:41 +0300

    Update Matrice_patratica.h

--- tema2/Matrice_patratica.h
+++ tema2/Matrice_patratica.h
@@ -1,26 +1,35 @@
 #pragma once
 #include <iostream>
 #include "Matrice.h"
-#ifndef TEMAPOO2_MATRICE_OARECARE_H
-#define TEMAPOO2_MATRICE_OARECARE_H
-class Matrice_oarecare :public Matrice
-{
-	int lin, col;
-    explicit Matrice_oarecare(int col = 1, int lin = 1) : Matrice(col, lin) {};
+#include "Complex.h"
+#ifndef TEMAPOO2_MATRICE_PATRATICA_H
+#define TEMAPOO2_MATRICE_PATRATICA_H
 
-    Matrice_oarecare(const Matrice_oarecare&);
 
-    ~Matrice_oarecare();
 
-    Matrice_oarecare& operator =(Matrice_oarecare& ob);
+class Matrice_patratica : public Matrice {
+    int dim;
+public:
+    explicit Matrice_patratica(int dim = 1);
 
-    friend std::ostream& operator <<(std::ostream& output, const Matrice_oarecare& mat);
+    Matrice_patratica(const Matrice_patratica&);
 
-    friend std::istream& operator >>(std::istream& input, Matrice_oarecare& mat);
+    ~Matrice_patratica();
+
+    Matrice_patratica& operator=( Matrice_patratica&);
+
+    friend std::ostream& operator <<(std::ostream& output, const Matrice_patratica& mat);
+
+    friend std::istream& operator >>(std::istream& input, Matrice_patratica& mat);
 
     int verifica_triunghiulara() override;
 
-    void verifica_diagonala() override;
+    void verifica_diagonala()override;
+
+    Complex det(Matrice_patratica,int);
 };
 
-#endif // !TEMAPOO2_MATRICE_OARECARE_H
+
+
+#endif // !TEMAPOO2_MATRICE_PATRATICA_H
+

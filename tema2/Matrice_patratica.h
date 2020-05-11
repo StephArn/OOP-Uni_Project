d3d625215ea24403a67e15754c53436fe84b802b
StephArn <34690734+StephArn@users.noreply.github.com>
2020-05-12 00:06:35 +0300

    Update Matrice_patratica.h

--- tema2/Matrice_patratica.h
+++ tema2/Matrice_patratica.h
@@ -8,15 +8,15 @@
 
 
 class Matrice_patratica : public Matrice {
-    int dim;
+ 
 public:
-    explicit Matrice_patratica(int col = 1) : Matrice(col, col) {};
+    explicit Matrice_patratica(int col = 1) : Matrice(col, col,Complex(0,0)) {};
 
     Matrice_patratica(const Matrice_patratica&) = default;;
 
     ~Matrice_patratica() = default;
 
-    Matrice_patratica& operator=(const Matrice_patratica& );
+    Matrice_patratica& operator=(const Matrice_patratica&) =default;
 
     //friend void swap(Matrice_patratica& a, Matrice_patratica& b);
 

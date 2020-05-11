b6f7713d6d4bcd96d8005f856cadcf987b13cb76
StephArn <34690734+StephArn@users.noreply.github.com>
2020-05-11 18:54:31 +0300

    Update TemaPOO2.cpp

--- tema2/TemaPOO2.cpp
+++ tema2/TemaPOO2.cpp
@@ -1,12 +1,13 @@
 #include <iostream>
-#include "Complex.h"
-#include "Matrice.h"
 #include "Matrice_patratica.h"
+#include "Matrice_oarecare.h"
+
+using namespace std;
 
 int main() {
-    Matrice_patratica* m = new Matrice_patratica();
-    std::cin >> *m;
-    std::cout << *m;
-    delete m;
-    return 0;
+   auto *m = new Matrice_patratica();
+   cin >> *m;
+   cout << *m;
+   delete m;
+   return 0;
 }

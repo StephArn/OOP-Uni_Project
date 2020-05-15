8e14900b59d70c5cd79bba0b5d086ee6b6932287
StephArn <34690734+StephArn@users.noreply.github.com>
2020-05-15 03:44:01 +0300

    Update TemaPOO2.cpp

--- tema2/TemaPOO2.cpp
+++ tema2/TemaPOO2.cpp
@@ -7,11 +7,19 @@ using namespace std;
 int main() {
    auto *m = new Matrice_patratica();
    auto* n = new Matrice_oarecare();
+   cout << "Matricea patratica:" << endl;;
    cin >> *m;
    cout << *m;
+   cout << endl;
+   cout << "Matricea oarecare:" << endl;;
    cin >> *n;
    cout << *n;
+   cout << endl;
+   cout << "Verificam daca matricea patratica e diagonala:" << endl;
    m->verifica_diagonala();
+   cout << endl;
+   cout << "Verificam daca matricea oarecare e diagonala:" << endl;
+   n->verifica_diagonala();
    delete m;
    delete n;
 

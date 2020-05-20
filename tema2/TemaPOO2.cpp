e0b45c05282025fe3812821aa77b83ccdba33c33
StephArn <34690734+StephArn@users.noreply.github.com>
2020-05-21 02:00:01 +0300

    Update TemaPOO2.cpp

--- tema2/TemaPOO2.cpp
+++ tema2/TemaPOO2.cpp
@@ -1,18 +1,21 @@
 #include <iostream>
+#include <fstream>
 #include "Matrice_patratica.h"
 #include "Matrice_oarecare.h"
 
 using namespace std;
 
+ifstream f("intrare.txt");
+
 int main() {
    auto *m = new Matrice_patratica();
    auto* n = new Matrice_oarecare();
-   cout << "Matricea patratica:" << endl;;
-   cin >> *m;
+   cout << "Matricea patratica:" << endl;
+   f >> *m;
    cout << *m;
    cout << endl;
-   cout << "Matricea oarecare:" << endl;;
-   cin >> *n;
+   cout << "Matricea oarecare:" << endl;
+   f >> *n;
    cout << *n;
    cout << endl;
    cout << "Verificam daca matricea patratica e diagonala:" << endl;

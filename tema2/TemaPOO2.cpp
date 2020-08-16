e641315e8228a9e67092248ced776128eefc0959
StephArn <34690734+StephArn@users.noreply.github.com>
2020-08-16 19:37:49 +0300

    Update TemaPOO2.cpp

--- tema2/TemaPOO2.cpp
+++ tema2/TemaPOO2.cpp
@@ -8,33 +8,39 @@ using namespace std;
 ifstream f("intrare.txt");
 
 int main() {
-   
-   Complex a, b;
-   f >> a >> b;
-   Complex c;
-   c = a + b;
-   c = a * b;
-   cout << c << endl;
-   cout << a.getReal() << endl;
-   cout << b.getImaginar() << endl;
+
+    Complex a, b;
+    f >> a >> b;
+    Complex c;
+    c = a + b;
+    c = a * b;
+    cout << c << endl;
+    cout << a.getReal() << endl;
+    cout << b.getImaginar() << endl;
+
+    int x;
+    f >> x;
+    Complex y;
+    y = x * c;
+    cout << y;
 
 
-   auto *m = new Matrice_patratica();
-   auto* n = new Matrice_oarecare();
-   cout << "Matricea patratica:" << endl;
-   f >> *m;
-   cout << *m;
-   cout << endl;
-   cout << "Matricea oarecare:" << endl;
-   f >> *n;
-   cout << *n;
-   cout << endl;
-   cout << "Verificam daca matricea patratica e diagonala:" << endl;
-   m->verifica_diagonala();
-   cout << endl;
-   cout << "Verificam daca matricea oarecare e diagonala:" << endl;
-   n->verifica_diagonala();
-   delete m;
-   delete n;
-   return 0;
+    auto* m = new Matrice_patratica();
+    auto* n = new Matrice_oarecare();
+    cout << "Matricea patratica:" << endl;
+    f >> *m;
+    cout << *m;
+    cout << endl;
+    cout << "Matricea oarecare:" << endl;
+    f >> *n;
+    cout << *n;
+    cout << endl;
+    cout << "Verificam daca matricea patratica e diagonala:" << endl;
+    m->verifica_diagonala();
+    cout << endl;
+    cout << "Verificam daca matricea oarecare e diagonala:" << endl;
+    n->verifica_diagonala();
+    delete m;
+    delete n;
+    return 0;
 }

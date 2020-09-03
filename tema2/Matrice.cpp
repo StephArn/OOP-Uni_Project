f5ca6f69cfd2a565a795e4890a238d48e8de84fb
StephArn <34690734+StephArn@users.noreply.github.com>
2020-09-04 01:34:18 +0300

    Update Matrice.cpp

--- tema2/Matrice.cpp
+++ tema2/Matrice.cpp
@@ -2,17 +2,19 @@
 #include "Matrice.h"
 #include "Complex.h"
 
+template <class T>
 
-Matrice::Matrice(int lin, int col, Complex nr) : lin{lin}, col{col} {
-    v = new Complex * [lin];
+Matrice<T>::Matrice(int lin, int col, Complex<T> nr) : lin{ lin }, col{ col } {
+    v = new Complex<T> * [lin];
     for (int i = 0; i < lin; i++)
-        v[i] = new Complex[col];
+        v[i] = new Complex<T>[col];
     for (int i = 0; i < lin; i++)
         for (int j = 0; j < col; j++)
             v[i][j] = nr;
 }
+template <class T>
 
-Matrice ::~Matrice() {
+Matrice<T> ::~Matrice() {
     for (int i = 0; i < lin; i++)
         delete[] v[i];
     delete[] v;
@@ -21,21 +23,25 @@ Matrice ::~Matrice() {
 
 }
 
-Matrice::Matrice(const Matrice &mat) {
+template <class T>
+
+Matrice<T>::Matrice(const Matrice<T>& mat) {
 
     lin = mat.lin;
     col = mat.col;
-    
-    v = new Complex * [lin];
+
+    v = new Complex<T> * [lin];
     for (int i = 0; i < lin; i++)
-        v[i] = new Complex[col];
+        v[i] = new Complex<T>[col];
     for (int i = 0; i < lin; i++)
         for (int j = 0; j < col; j++)
             v[i][j] = mat.v[i][j];
 
 }
 
-Matrice& Matrice::operator =(const Matrice&  ob) {
+template <class T>
+
+Matrice<T>& Matrice<T>::operator =(const Matrice<T>& ob) {
 
     if (this == &ob) return *this;
 
@@ -45,45 +51,62 @@ Matrice& Matrice::operator =(const Matrice&  ob) {
 
     this->lin = ob.lin;
     this->col = ob.col;
-    for(int i = 0; i < lin; i++)
+    for (int i = 0; i < lin; i++)
         for (int j = 0; j < col; j++)
             this->v[i][j] = ob.v[i][j];
     return *this;
 }
 
-std::istream& operator >>(std::istream& input, Matrice& mat) {
+template <class T>
+
+std::istream& operator >>(std::istream& input, Matrice<T>& mat) {
 
     for (int i = 0; i < mat.lin; i++)
         delete[] mat.v[i];
     delete[] mat.v;
 
     input >> mat.col >> mat.lin;
-    mat.v = new Complex * [mat.lin];
+    mat.v = new Complex<T> * [mat.lin];
     for (int i = 0; i < mat.lin; i++)
-        mat.v[i] = new Complex[mat.col];
+        mat.v[i] = new Complex<T>[mat.col];
     for (int i = 0; i < mat.lin; i++)
         for (int j = 0; j < mat.col; j++)
             input >> mat.v[i][j];
     return input;
 }
 
-std::ostream& operator <<(std::ostream& output, const Matrice& mat) {
+template <class T>
+
+std::ostream& operator <<(std::ostream& output, const Matrice<T>& mat) {
     for (int i = 0; i < mat.lin; i++) {
         for (int j = 0; j < mat.col; j++)
             output << mat.v[i][j] << " ";
         output << '\n';
     }
     return output;
-} 
+}
 
-int Matrice::get_lin() {
+template <class T>
+
+int Matrice<T>::get_lin() {
     return lin;
 }
 
-int Matrice::get_col() {
+template <class T>
+
+int Matrice<T>::get_col() {
     return col;
 }
 
-Complex** Matrice::get_mat() {
+template <class T>
+
+Complex<T>** Matrice<T>::get_mat() {
     return v;
+}
+
+template <class T>
+
+Complex<T> Matrice<T>::setElem(int i, int j, Complex<T> x)
+{
+    v[i][j] = x;
 }

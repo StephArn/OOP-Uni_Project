4f468a66def7bac8f1261b10465ccbc40d215dd3
StephArn <34690734+StephArn@users.noreply.github.com>
2020-09-04 01:33:50 +0300

    Update Matrica_oarecare.cpp

--- tema2/Matrica_oarecare.cpp
+++ tema2/Matrica_oarecare.cpp
@@ -1,19 +1,25 @@
 #include "Matrice_oarecare.h"
 
-std::ostream& operator <<(std::ostream& output, const Matrice_oarecare& mat) {
+template <class T>
+
+std::ostream& operator <<(std::ostream& output, const Matrice_oarecare<T>& mat) {
     for (int i = 0; i < mat.lin; i++) {
         for (int j = 0; j < mat.col; j++)
             output << mat.v[i][j] << " ";
         output << '\n';
     }
     return output;
+
 }
-int Matrice_oarecare::verifica_triunghiulara() {
+
+template <class T>
+
+int Matrice_oarecare<T>::verifica_triunghiulara() {
     int i, j, ok1 = 1, ok2 = 1, rez;
     if (lin != col) return 0;
     for (i = 1; i < lin; i++) {
         for (j = 0; j < i; j++) {
-            if (v[i][j] != Complex(0, 0)) {
+            if (v[i][j] != Complex<T>(0, 0)) {
                 ok1 = 0;
                 break;
             }
@@ -23,7 +29,7 @@ int Matrice_oarecare::verifica_triunghiulara() {
 
     for (i = 0; i < lin - 1; i++) {
         for (j = i + 1; j < lin; j++) {
-            if (v[i][j] != Complex(0, 0)) {
+            if (v[i][j] != Complex<T>(0, 0)) {
                 ok2 = 0;
                 break;
             }
@@ -41,7 +47,9 @@ int Matrice_oarecare::verifica_triunghiulara() {
     return rez;
 }
 
-void Matrice_oarecare::verifica_diagonala() {
+template <class T>
+
+void Matrice_oarecare<T>::verifica_diagonala() {
     int triunghiulara;
     triunghiulara = verifica_triunghiulara();
     if (triunghiulara == 3)

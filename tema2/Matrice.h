40c8951d18f1f367c2b4e816aefb3204e784bfdd
StephArn <34690734+StephArn@users.noreply.github.com>
2020-09-04 20:13:56 +0300

    Update Matrice.h

--- tema2/Matrice.h
+++ tema2/Matrice.h
@@ -4,34 +4,37 @@
 
 
 #include "Complex.h"
+template <class T>
 class Matrice
 {
 protected:
-    Complex **v;
+    Complex<T>** v;
     int col, lin;
 
 public:
-    explicit Matrice(int col = 0, int lin = 0, Complex nr = Complex(0, 0));
+    explicit Matrice(int col = 0, int lin = 0, Complex<T> nr = Complex<T>(0, 0));
 
-    Matrice(const Matrice& mat);
+    Matrice(const Matrice<T>& mat);
 
     virtual ~Matrice();
 
-    friend std::ostream& operator <<(std::ostream&, const Matrice&);
+    friend std::ostream& operator <<(std::ostream&, const Matrice<T>&);
 
-    friend std::istream& operator >>(std::istream&, Matrice&);
+    friend std::istream& operator >>(std::istream&, Matrice<T>&);
 
-    Matrice& operator =(Matrice const &ob);
+    Matrice& operator =(Matrice<T> const& ob);
 
-    virtual int verifica_triunghiulara()=0;
+    virtual int verifica_triunghiulara() = 0;
 
-    virtual void verifica_diagonala()=0;
+    virtual void verifica_diagonala() = 0;
+
+    Complex<T> setElem(int i, int j, Complex<T> x);
 
     int get_lin();
     int get_col();
-    Complex** get_mat();
+    Complex<T>** get_mat();
 
-    
+
 
 };
 

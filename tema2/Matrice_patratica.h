fbcb74d2c9c774305337a7f073228d46719d7ea0
StephArn <34690734+StephArn@users.noreply.github.com>
2020-09-04 20:16:12 +0300

    Update Matrice_patratica.h

--- tema2/Matrice_patratica.h
+++ tema2/Matrice_patratica.h
@@ -5,27 +5,27 @@
 #ifndef TEMAPOO2_MATRICE_PATRATICA_H
 #define TEMAPOO2_MATRICE_PATRATICA_H
 
+template <class T>
 
-
-class Matrice_patratica : public Matrice
+class Matrice_patratica : public Matrice<T>
 {
 
 public:
-    explicit Matrice_patratica(int lin = 0, int col = 0) : Matrice(lin, col) {};
+    explicit Matrice_patratica(int lin = 0, int col = 0) : Matrice<T>(lin, col) {};
 
-    Matrice_patratica(const Matrice_patratica&) = default;
+    Matrice_patratica(const Matrice_patratica<T>&) = default;
 
-    ~Matrice_patratica() override = default;
+    ~Matrice_patratica<T>() override = default;
 
-    Matrice_patratica& operator=(const Matrice_patratica&) = default;
+    Matrice_patratica<T>& operator=(const Matrice_patratica<T>&) = default;
 
-    friend std::ostream& operator <<(std::ostream& output, Matrice_patratica& mat);
+    friend std::ostream& operator <<(std::ostream& output, Matrice_patratica<T>& mat);
 
     int verifica_triunghiulara() override;
 
     void verifica_diagonala()override;
 
-    friend Complex det(Matrice_patratica& matrix, int n);
+    friend Complex<T> det(Matrice_patratica<T>& matrix, int n);
 
 };
 
